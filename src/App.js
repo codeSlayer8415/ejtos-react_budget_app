47087ca7f4a3148e69932e30b410cb0a0f131394
Vrudhula Sreedhar <vrudhulasreedhar@gmail.com>
2024-06-26 21:49:59 -0400

    Updated budget and remaining.js files

--- src/App.js
+++ src/App.js
@@ -6,36 +6,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
-
+import Remaining from './components/Remaining';
 import { AppProvider } from './context/AppContext';
+//import Remaining from './components/Remaining';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
+                        {
                             /* Add Budget component here */
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
-                        {
+                        {
                             /* Add Remaining component here*/
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
-                        {
+                        {
                             /* Add ExpenseTotal component here */
                         }        
                        
-                        {
+                        {
                             /* Add ExpenseList component here */
                         }         
 
-                        {
+                        {
                             /* Add ExpenseItem component here */
                         }        
 
-                        {
+                        {
                             /* Add AllocationForm component here under */
                         }        
 
