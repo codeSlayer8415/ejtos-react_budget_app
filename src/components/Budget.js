42f1dc5310521201f66d2384b25024e8a90308e1
Vrudhula Sreedhar <vrudhulasreedhar@gmail.com>
2024-07-08 06:50:41 -0400

    Updated Budget compnent to update budget redux state

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,10 +3,26 @@ import {AppContext} from '../context/AppContext';
 
 
 const Budget=()=>{
-    let {budget}=useContext(AppContext);
+    let {budget,dispatch,remaining}=useContext(AppContext);
     const [newBudget,setNewBudget]=useState(budget);
     const handleBudgetChange=(event)=>{
         setNewBudget(event.target.value);
+        if(newBudget>20000)
+        {
+            alert(`Budget cannot exceed 20000, remaining funds ${remaining}`)
+        }
+        else if(newBudget<budget-remaining)
+        {
+            alert("You cannot reduce the budget lower than spending");
+        }
+        else
+        {
+            dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget,
+            });
+        }
+        
     }
     return(
         <div className='alert alert-secondary'>
